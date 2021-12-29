8d999efa157d5a429e9b51e0c1d3aedcf21775a8
Jack Gumoes <j.gumoes@gmail.com>
2021-12-29 16:50:05 +0000

    came up with a recursive OOP approach to make the calculator handle fractions a lot easier. haven't implemented any of it, but  wrote some notes about it.

--- lib/Calculator.ts
+++ lib/Calculator.ts
@@ -1,5 +1,83 @@
 import { Console } from "console"
 
+// TODO: create a simple history class that contains but limits the number of previous answers
+
+/*
+okay so here me out, what if, what IF, the inputs list is actually a list of objects representing each
+operation?
+
+for example, there would be a number class that can store and return the numbers, but also parse them
+into a string i.e. if the class stores 399981, then Number.print() returns "399,981".
+Number.add(".") could add a decimal, or, if the decimal is given a custom button, Number.dec() could add
+the decimal.
+
+the operators +-x/ could have their own class initiated with their type.
+
+the fraction class should work like the Calculator class, storing and manipulating inputs in the same way.
+This would allow me to easily nest fractions to any arbitrary limit, since the fractions will be evaluated
+identically to the calculator class. Infact, the fractions class could contain two instances of Calculator
+named numerator and denominator (or top and bottom). the fraction class could have the method:
+.answer() { return self.top.answer() / self.bottom.answer()}
+
+there is another potential benefit: if each class is responsible for rendering itself, rendering the entire
+screen would be pretty straitforward.
+
+all the classes should have a .del() function that removes the last input. it would return true if successful,
+but false if it wasn't able to and therefore must be deleted (i.e. if Number only has one character).
+
+each class would need to return their length so the App knows where to insert the curser.
+.length(op=0) {return 0 + self.length}
+
+Calculator{
+  self._length
+  self.inputs = []
+  
+  public:
+    function add()
+    function del() {if not inputs[-1].del() {inputs.remove(-1)}}
+    function answer() // travels self.inputs from left to right, creating a running total
+    function plus()
+    function minus()
+    function times()
+    // function divide() // actually probably not this one
+    function equals()
+    function fraction()
+    function length( l = 0 ) { return self._length + l } // this should make it easy finding the curser position
+  
+  private:
+    function insertInput(input) {
+      if self.inputs[-1].addValue() == false:
+        self.inputs.append(input)
+    }
+}
+
+Plus{
+  function add(input) { return false }
+  function operate( a, b ) { return a + b }
+  function del() { return false }
+  function length( l = 0) { return 1 } probably not like this
+}
+
+NumberClass{
+  self.value = ""
+
+  function add(input) { self.value += input; return true } // input should only be numbers
+  function dec() { self.value += "." }
+  function number() { return Number(self.value)}
+  function print() // turns 399981 into 399,981
+}
+
+Fraction{
+  self.top
+  self.bottom
+  
+  public:
+    
+}
+
+*/
+
+// TODO: change from class to module (unless I do the above in which case I might want to use inheritance)
 export default class Calculator {
   inputs: string[]
 
